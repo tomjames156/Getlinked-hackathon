dbb2621acf4e5a03baff2c65442489f9ee96c8ec
Akinwande Tomisin <tomjames156@gmail.com>
2023-09-22 19:33:07 +0100

    added countdown timer component

---
+++ getlinked-site/src/components/CountdownTimer.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+
+function CountdownTimer() {
+  const targetDate = new Date('November 18, 2023 00:00:00').getTime();
+  const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining());
+
+  function calculateTimeRemaining() {
+    const now = new Date().getTime();
+    const difference = targetDate - now;
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    return {
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeRemaining(calculateTimeRemaining());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <p className='countdown'>
+        {timeRemaining.days > 10 ? timeRemaining.days :'0'+timeRemaining.days}<span>D</span>
+        {timeRemaining.hours > 10 ? timeRemaining.hours : '0'+ timeRemaining.hours}<span>H</span>
+        {timeRemaining.minutes > 10 ? timeRemaining.minutes : '0' + timeRemaining.minutes}<span>M</span>
+        {timeRemaining.seconds > 10 ? timeRemaining.seconds : '0' + timeRemaining.seconds}<span>S</span>
+    </p>
+  );
+}
+
+export default CountdownTimer;
