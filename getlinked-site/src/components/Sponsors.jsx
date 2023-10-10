8d231a2fc3bec6f17551f4cfbdb8f1fb619620f5
Akinwande Tomisin <tomjames156@gmail.com>
2023-10-10 22:16:49 +0100

    updated sponsors component table

--- getlinked-site/src/components/Sponsors.jsx
+++ getlinked-site/src/components/Sponsors.jsx
@@ -8,68 +8,66 @@ import { motion } from 'framer-motion'
 
 function Sponsors() {
   return (
-    <>
-        <div>
-            <motion.img
-                src={libertyWhiteLogo}
-                alt="liberty assured company"
-                initial={{ x: -100, opacity: 0}}
-                whileInView={{ x: 0, opacity: 1, transition: { duration: 0.5 }}}
-                viewport={{once: true, amount: 0.1 }}
-            />
-        </div>
-        <div>
-            <motion.img
-                src={libertyPayLogo}
-                alt="liberty pay company"
-                initial={{ opacity: 0}}
-                whileInView={{opacity: 1, transition: { duration: 0.5 }}}
-                viewport={{once: true, amount: 0.1 }}
-            />
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-        <div>
-            <motion.img
-                src={winwiseLogo}
-                alt="winwise company"
-                initial={{ y: -150, opacity: 0}}
-                whileInView={{ y: 0, opacity: 1, transition: { duration: 0.5 }}}
-                viewport={{once: true, amount: 0.1 }}
-            />
-        </div>
-        <div>
-            <motion.img
-                src={wisperLogo}
-                alt="wisper sms"
-                initial={{ x: -100, opacity: 0}}
-                whileInView={{ x: 0, opacity: 1, transition: { duration: 0.5 }}}
-                viewport={{once: true, amount: 0.1 }}
-            />
-        </div>
-        <div>
-            <motion.img
-                src={payboxLogo}
-                alt="pay box company"
-                initial={{ y: 150, opacity: 0}}
-                whileInView={{ y: 0, opacity: 1, transition: { duration: 0.5 }}}
-                viewport={{once: true, amount: 0.1 }}
-            />
-            <span></span>
-            <span></span>
-        </div>
-        <div>
-            <motion.img
-                src={vuzualPlusLogo}
-                alt="vuzual plus design studios"
-                initial={{ x: 100, opacity: 0}}
-                whileInView={{ x: 0, opacity: 1, transition: { duration: 0.5 }}}
-                viewport={{once: true, amount: 0.1 }}
-            />
-        </div>
-    </>
+    <table>
+        <tr>
+            <td>
+                <motion.img
+                    src={libertyWhiteLogo}
+                    alt="liberty assured company"
+                    initial={{ x: -100, opacity: 0}}
+                    whileInView={{ x: 0, opacity: 1, transition: { duration: 0.5 }}}
+                    viewport={{once: true, amount: 0.1 }}
+                />
+            </td>
+            <td>
+                <motion.img
+                    src={libertyPayLogo}
+                    alt="liberty pay company"
+                    initial={{ opacity: 0}}
+                    whileInView={{opacity: 1, transition: { duration: 0.5 }}}
+                    viewport={{once: true, amount: 0.1 }}
+                />
+            </td>
+            <td>
+                <motion.img
+                    src={winwiseLogo}
+                    alt="winwise company"
+                    initial={{ y: -150, opacity: 0}}
+                    whileInView={{ y: 0, opacity: 1, transition: { duration: 0.5 }}}
+                    viewport={{once: true, amount: 0.1 }}
+                />
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <motion.img
+                    src={wisperLogo}
+                    alt="wisper sms"
+                    initial={{ x: -100, opacity: 0}}
+                    whileInView={{ x: 0, opacity: 1, transition: { duration: 0.5 }}}
+                    viewport={{once: true, amount: 0.1 }}
+                />
+            </td>
+            <td>
+                <motion.img
+                    src={payboxLogo}
+                    alt="pay box company"
+                    initial={{ y: 150, opacity: 0}}
+                    whileInView={{ y: 0, opacity: 1, transition: { duration: 0.5 }}}
+                    viewport={{once: true, amount: 0.1 }}
+                />
+            </td>
+            <td>
+                <motion.img
+                    src={vuzualPlusLogo}
+                    alt="vuzual plus design studios"
+                    initial={{ x: 100, opacity: 0}}
+                    whileInView={{ x: 0, opacity: 1, transition: { duration: 0.5 }}}
+                    viewport={{once: true, amount: 0.1 }}
+                />
+            </td>
+        </tr>
+    </table>
   )
 }
 
